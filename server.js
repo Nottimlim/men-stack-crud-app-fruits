627fdf5571f20e04cd8ddc213e6002f30a0eb479
Timothy Lim <contacttimothylim@gmail.com>
2024-09-25 18:12:50 -0400

    commit

--- server.js
+++ server.js
@@ -2,15 +2,21 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express =  require ("express"); 
-const mongoose = require("mongoose");
-const Fruit = require("./models/fruit.js");
 const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const Fruit = require("./models/fruit.js");
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 })
+
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+
 app.listen(3000, () => {console.log("listening at 3000")});
 
 // GET /
